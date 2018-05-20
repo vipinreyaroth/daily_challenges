74b800fe49e76a5300a624292d72fec27fe04427
vipinreyaroth <vipinreyaroth@gmail.com>
2018-05-20 21:06:27 +1000

    Completed the challenge after refactoring code to include weather data parsing functionality

--- day1/parse_csv.py
+++ day1/parse_csv.py
@@ -1,39 +1,40 @@
-import csv
-
 '''
-Write a program to parase a csv file (s) and obtain the item with min score difference. 
-
+Write a program to parse a csv file (s) and obtain the item with min score difference.
 Problem
-Problem
-Football: The football.csv file contains the results from the English Premier League. The columns labeled ‘Goals’ and 
-‘Goals Allowed’ contain the total number of goals scored for and against each team in that season (so Arsenal scored 
+Football: The football.csv file contains the results from the English Premier League. The columns labeled 'Goals' and 
+'Goals Allowed' contain the total number of goals scored for and against each team in that season (so Arsenal scored 
 79 goals against opponents, and had 36 goals scored against them). Write a program to read the file, then print the 
-name of the team with the smallest difference in ‘for’ and ‘against’ goals.
+name of the team with the smallest difference in 'for' and 'against' goals.
 
-Weather: In weather.csv you’ll find daily weather data. Write a program to read the file, then output the day number 
+Weather: In weather.csv you'll find daily weather data. Write a program to read the file, then output the day number 
 (column one) with the smallest temperature spread (the maximum temperature is the second column, the minimum the 
 third column).
 See if you can write the same program to solve both questions.
 '''
 
-
-def read_csv(file_name):
-    with open(file_name, 'r') as f:
-        return [row for row in csv.reader(f)]
+import csv
 
 
-def get_min_score_difference(parsed_data):
-    parsed_data.pop(0)
-    goals_scored = [x[5] for x in parsed_data]
-    goals_allowed = [x[6] for x in parsed_data]
+class ParseCsv(object):
+    def __init__(self, file_name):
+        self.file_name = file_name
 
-    print(goals_allowed)
-    print(goals_scored)
-    values = [int(x) - int(y) for x, y in zip(goals_scored, goals_allowed)]
-    print(values)
-    return values.index(min(values))
+    def read_data(self):
+        with open(self.file_name, 'r') as f:
+            return [row for row in csv.reader(f)]
 
+    def get_min_score_difference(self, parsed_data, column1, column2):
+        parsed_data.pop(0)
+        goals_scored = [x[column1] for x in parsed_data]
+        goals_allowed = [x[column2] for x in parsed_data]
 
-def get_team(index, parsed_data):
-    teams = [x[0] for x in parsed_data]
-    return teams[index]
+        values = [int(x) - int(y) for x, y in zip(goals_scored, goals_allowed)]
+        return values.index(min(values))
+
+    def get_team(self, index, parsed_data):
+        teams = [x[0] for x in parsed_data]
+        return teams[index]
+
+    def get_day(self, index, parsed_data):
+        days = [x[0] for x in parsed_data]
+        return days[index]
--- day1/test_parse_csv.py
+++ day1/test_parse_csv.py
@@ -5,30 +5,48 @@ In this iteration, we will test if the program is able to parse the 'football.cs
 The tests will check the header, and the some data to test if the data is read correctly.
 '''
 
-from parse_csv import read_csv, get_min_score_difference, get_team
+from parse_csv import ParseCsv
+import unittest
 
-FILE_NAME = 'football.csv'
-parsed_data = [['Team', 'Games', 'Wins', 'Losses', 'Draws', 'Goals', 'Goals Allowed', 'Points'],
+class ParseCSVTest(unittest.TestCase):
+    def setUp(self):
+        self.football_data = 'football.csv'
+        self.weather_data = 'weather.csv'
+        self.football = ParseCsv(self.football_data)
+        self.weather = ParseCsv(self.weather_data)
+
+        self.parsed_data_football = [['Team', 'Games', 'Wins', 'Losses', 'Draws', 'Goals', 'Goals Allowed', 'Points'],
                ['Arsenal', '38', '26', '9', '3', '79', '36', '87'],
                ['Liverpool', '38', '24', '8', '6', '67', '30', '80']]
+        self.parsed_data_weather = [['Day', 'MxT', 'MnT', 'AvT', 'AvDP', '1HrP TPcpn', 'PDir', 'AvSp', 'Dir', 'MxS',
+                                    'SkyC', 'MxR', 'Mn', 'R AvSLP'],
+        ['1', '88', '59', '74', '53.8', '0', '280', '9.6', '270', '17', '1.6', '93', '23', '1004.5'],
+        ['2', '79', '63', '71', '46.5', '0', '330', '8.7', '340', '23', '3.3', '70', '28', '1004.5']]
 
-def test_csv_read_data_headers():
-    assert read_csv(FILE_NAME)[0] == ['Team', 'Games', 'Wins', 'Losses', 'Draws', 'Goals',
-                                                  'Goals Allowed', 'Points']
+    def test_csv_read_data_headers(self):
+        self.assertEqual(self.football.read_data()[0], ['Team', 'Games', 'Wins', 'Losses', 'Draws', 'Goals',
+                                                       'Goals Allowed', 'Points'])
+        self.assertEqual(self.weather.read_data()[0], ['Day', 'MxT', 'MnT', 'AvT', 'AvDP', '1HrP TPcpn', 'PDir',
+                                                        'AvSp', 'Dir', 'MxS', 'SkyC', 'MxR', 'Mn', 'R AvSLP'])
 
+    def test_read_data_team_name(self):
+        self.assertEqual(self.football.read_data()[1][0], 'Arsenal')
 
-def test_csv_read_data_team_name():
-    assert read_csv(FILE_NAME)[1][0] == 'Arsenal'
+    def test_read_data_day(self):
+        self.assertEqual(self.weather.read_data()[1][0], '1')
 
+    def test_read_data_points(self):
+        self.assertEqual(self.football.read_data()[1][7], '87')
+        self.assertEqual(self.weather.read_data()[1][1], '88')
 
-def test_csv_read_data_points():
-    assert read_csv(FILE_NAME)[1][7] == '87'
+    def test_get_min_score_difference(self):
+        self.assertEqual(self.football.get_min_score_difference(self.parsed_data_football, 5, 6), 1)
+        self.assertEqual(self.weather.get_min_score_difference(self.parsed_data_weather, 2, 3), 0)
 
+    def test_get_team(self):
+        self.assertEqual(self.football.get_team(self.football.get_min_score_difference(self.parsed_data_football, 5, 6),
+                                                self.parsed_data_football), 'Liverpool')
 
-def test_get_min_score_difference():
-    assert get_min_score_difference(parsed_data) == 1
-
-
-def test_get_team():
-    assert get_team(get_min_score_difference(parsed_data), parsed_data) == 'Liverpool'
-
+    def test_get_day(self):
+        self.assertEqual(self.weather.get_day(self.weather.get_min_score_difference(self.parsed_data_weather, 2, 3),
+                                              self.parsed_data_weather), '1')
