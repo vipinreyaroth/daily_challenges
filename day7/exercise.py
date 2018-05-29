8fcb81424bba2ce2800da90604de6a7b4e136f96
vipinreyaroth <38558860+vipinreyaroth@users.noreply.github.com>
2018-05-29 17:23:03 +1000

    Added the reference to Pybites

--- day7/exercise.py
+++ day7/exercise.py
@@ -1,13 +1,14 @@
 """
-This exercise is to explain the use of properties
+This exercise is to explain the use of properties. This is based on Pybytes article by Bob Belderos.
 """
 from account import Account
 
+
 def main():
-   acc = Account('Vipin', 200)
-   acc += 400
-   acc -= 100
-   print(acc)
+    acc = Account('Vipin', 200)
+    acc += 400
+    acc -= 100
+    print(acc)
 
 
 if __name__ == '__main__':
